fabf52b4087cc0d2acca366550ea156d692c35a8
Domagoj Žganec <domagoj.zganec1@gmail.com>
2016-02-22 10:52:10 +0100

    Edited navigation and footer options

--- footer.php
+++ footer.php
@@ -4,12 +4,26 @@
 <div class="container">
 
 
-<div class="company_name">
- Company name
-</div>
-<div class="address">
- Example Address, 1807 Glenwood St NE, Palm Bay FL 32907
-</div>
+
+
+
+
+    <?php if(get_field('footer_company_name', 'options') ): ?>
+        <div class="company_name">
+            <?php the_field('footer_company_name', 'options'); ?>
+        </div>
+  <?php endif; ?>
+
+
+
+
+    <?php if(get_field('footer_address_descr', 'options') ): ?>
+        <div class="address">
+            <?php the_field('footer_address_descr', 'options'); ?>
+        </div>
+    <?php endif; ?>
+
+
   <?php if( have_rows('header_contact_icons', 'options') ): ?>
 
     <div class="footer_contact">
--- header.php
+++ header.php
@@ -7,6 +7,8 @@
     <meta name="description" content="">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/min/style.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <header class="siteHeader">
